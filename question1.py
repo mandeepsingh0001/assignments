afc521c336f1b1e5a60a9b2173c1dcdb280eeb9b
mandeepsingh0001 <mandeep0107@gmail.com>
2017-08-16 11:25:53 +0530

    factorial of number

---
+++ question1.py
@@ -0,0 +1,26 @@
+for x in range (2000 , 3200):
+    if (x % 7) == 0:
+        y = x/5.0
+        if (y % 5)!= 0 :
+            print str(x) + ",",
+
+
+# question number 2
+
+num = raw_input("enter the number")
+print (num)
+factorial = 1
+
+if num < 0:
+    print("sorry factorial does not exist for negative numbers")
+elif num == 0:
+    print("the factorial of 0 is 1")
+else:
+    for i in range(1 , num + 1):
+        factorial = factorial * i
+        print ("the factorial of num is",factorial)
+
+
+
+
+
