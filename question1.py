799c33a3b23f4832867a143b603bd7c63ad2368d
mandeepsingh0001 <mandeep0107@gmail.com>
2017-08-17 10:47:34 +0530

    files are added

--- question1.py
+++ question1.py
@@ -1,4 +1,11 @@
+for x in range (2000 , 3200):
+    if (x % 7) == 0:
+        y = x/5.0
+        if (y % 5)!= 0 :
+            print str(x) + ",",
 
+
+# question number 2
 
 num = raw_input("enter the number")
 print (num)
@@ -14,6 +21,10 @@ else:
         print ("the factorial of num is",factorial)
 
 
+# question 3
 
-
-
+def squares (n) :
+    result = {}
+    for i in range(1 , n+1):
+        result[i] = i*i
+        return result
